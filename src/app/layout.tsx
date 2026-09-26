5223501ef219b8ad7db408e898039062f002e65d
Lycexia <lycexia@gmail.com>
2026-09-26 13:20:16 +0300

    feat: add vercel analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
 
